8cfabe384e99698e2338004799c8e81d4ec3892a
啊旺 <986870079@qq.com>
2020-05-11 15:09:34 +0800

    Update LearningRate.py

--- PyTorchProj/src/LearningRate.py
+++ PyTorchProj/src/LearningRate.py
@@ -171,6 +171,7 @@ torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max, eta_min=0, last_epo
              )
         validate(...)        
         scheduler.step()
+        print(scheduler.last_epoch, l.get_lr()[0])
 
 # T_max(int)- 一次学习率周期的迭代次数，即 T_max 个 epoch 之后重新设置学习率。
 # eta_min(float)- 最小学习率，即在一个周期中，学习率最小会下降到 eta_min，默认值为 0。
