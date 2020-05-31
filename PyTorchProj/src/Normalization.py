51431adf16a71a28d7eeb10146b818f2bd8a810c
啊旺 <986870079@qq.com>
2020-05-31 16:48:43 +0800

    Update Normalization.py

--- PyTorchProj/src/Normalization.py
+++ PyTorchProj/src/Normalization.py
@@ -3,5 +3,9 @@
 ————————————————  https://blog.csdn.net/liuxiao214/article/details/81037416
 
 1. Batch Normalization : N H W  nn.BatchNorm2d
+  
 2. Layer Normalization : C H W  nn.LayerNorm
+  实际实现是permute N and C , 通过BatchNorm间接完成再permute回来。
+  
 3. Instance Normalization : H W  nn.InstanceNorm2d
+  实际实现是view 1 N*C H W , 通过BatchNorm间接完成再view回来。
